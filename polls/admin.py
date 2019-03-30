1de0eacd38c5fc727a0d9ccc300a04cf2b3b3443
alexsim <jlmaj_sim@naver.cim>
2019-03-30 11:07:46 +0900

    alexsim

--- polls/admin.py
+++ polls/admin.py
@@ -1,4 +1,26 @@
 from django.contrib import admin
-from .models import Question
+from .models import Question, Choice
+from django.utils import timezone
+from datetime import datetime
 
-admin.site.register(Question)
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields' : ['question_text']}),
+        ('Date Information', {'fields' : ['pub_date']})
+    ]
+
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+
+    inlines = [ChoiceInline]
+
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+
+
+admin.site.register(Question, QuestionAdmin)
+#admin.site.register(Question) 
+#admin.site.register(Choice)
--- polls/models.py
+++ polls/models.py
@@ -14,6 +14,10 @@ class Question(models.Model):
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently'
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
@@ -21,4 +25,4 @@ class Choice(models.Model):
     votes = models.IntegerField(default=0)
 
     def __str__(self):
-        return self.question
+        return self.choice_text
